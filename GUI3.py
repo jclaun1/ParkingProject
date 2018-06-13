7c75d6912a1ac4bba8682f2a2b46fd83f01baef0
Pearl Pongsuppat <39709112+ppongs1@users.noreply.github.com>
2018-06-12 23:24:46 -0400

    Update GUI3.py

--- GUI3.py
+++ GUI3.py
@@ -1,44 +1,128 @@
-# test_mouse.py                                                                                                           
-from tkinter import *
-root=Tk()
-root.title('test_mouse.py')
-print ('test_mouse.py window gets mouse x,y')
+from tkinter import*
+from tkinter import messagebox
+from PIL import Image,ImageTk
+from tkinter import filedialog
+import os
 
-def mouseMove(event):
-  print ('mouse move x=')
-  print (event.x)
-  print (', y=')
-  print (event.y)
+def browse_button():
+    global File
+    File = filedialog.askopenfilename(parent=root, initialdir="M:/", title='Choose an image.')
+    print("opening %s" % File)
+    global w, img, imgWidth, imgHeight,original
+    w = Canvas(root, width=1920, height=1200)
+    w.pack()
 
-def mouseB1press(event): # left                                                                                           
-  print ('mouse B1 press x=')
-  print (event.x)
-  print (', y=')
-  print (event.y)
+    #Displays image on tkinter canvas                                                                                                                                                                             
+    print("opening %s" % File)
+    original = Image.open(File)
+#    original = original.resize((1920,1600)) #resize image                                                                                                                                                        
+    img = ImageTk.PhotoImage(original)
+    w.image = img
+    w.create_image(0, 0, image=img, anchor="nw")
+    return
 
-def mouseB1up(event):
-  print ('mouse B1 up x=')
-  print (event.x)
-  print (', y=')
-  print (event.y)
+def crop_button():
 
-def mouseB3press(event): # right                                                                                          
-  print ('mouse B3 press x=')
-  print (event.x)
-  print (', y=')
-  print (event.y)
+    w.bind("<Button 1>", printcoords)
+    return
 
-def keyInput(event):
-  print ("key input ", repr(event.char))
+def printcoords(event):
 
-c=Canvas(root, width=400, height=400, bg='white')
-c.create_rectangle(20,20,50,100,fill='blue')
-c.bind('<B1-Motion>'      , mouseMove)
-c.bind('<ButtonPress-1>'  , mouseB1press)
-c.bind('<ButtonRelease-1>', mouseB1up)
-c.bind('<ButtonPress-3>'  , mouseB3press)
+    global count,xVals, yVals, spotLabels, currentLabel
+    #Mouse clicking event                                                                                                                                                                                         
+    xVals.append(event.x)
+    yVals.append(event.y)
+    count = count + 1
+    i = 0
+    if (count % 2) == 0:
 
-c.pack()
+        if messagebox.askyesno("Title", "Is this a Parking Space?") == True:
+            currentLabel = 1
+            spotLabels.append(currentLabel)
+        else:
+            currentLabel = 0
+            spotLabels.append(currentLabel)
+
+        area = (xVals[i],yVals[i] , xVals[i+1], yVals[i+1])
+        i += 1
+        image = Image.open(File)
+        cropped_img = image.crop(area)
+        #filename, file_extension = os.path.splitext(File)                                                                                                                                                        
+
+        cropped_img.show()
+        saveimage = "test" + str(i) + '.JPG'
+        cropped_img.save(saveimage)
+        print("save image: " , saveimage)
+
+def exit():
+    w.unbind("<Button 1>")
+
+
+    #Array of SPOTS (arrays)                                                                                                                                                                                      
+    fileData = []
+
+    #Original Spot Number = -1      --  Check nested for loop for explanation                                                                                                                                     
+    spotIndex = -1
+
+    numberLabel = -1
+    #Writing to a python file now for easy data transfer to other files                                                                                                                                           
+    newFile = open('data_setup.py', 'w')
+
+    print (xVals)
+    print (yVals)
+    print (spotLabels)
+
+    print(count)
+    for i in range(count):
+
+        #To each individual spot, will add x0 and y0, followed by xf and yf upon next loop iteration                                                                                                              
+        if (i % 2) == 0:
+            spotIndex += 1
+            print ("spot index = ", spotIndex)
+            print("fileData = ", fileData)
+            #Create a new spot with no value                                                                                                                                                                      
+            fileData.append([])
+            if spotLabels[spotIndex] == 1:
+                numberLabel += 1
+                fileData[spotIndex].append(1)
+                fileData[spotIndex].append(numberLabel+1)
+            elif spotLabels[spotIndex] == 0:
+                fileData[spotIndex].append(0)
+                fileData[spotIndex].append(spotLabels[spotIndex])
+
+        fileData[spotIndex].append(xVals[i])
+        fileData[spotIndex].append(yVals[i])
+        print("NEW fileData = ", fileData)
+    #repr(fileData) takes the 2D array and converts it into string format to be written to the file                                                                                                               
+    newFile.write("boxes = " + repr(fileData) + "\n")
+    newFile.close()
+
+    quit()
+
+
+#Global variable 'spotLabel' == { 0 if control space | 1 if parking space}                                                                                                                                        
+spotLabels = [] #Array of labels for each click                                                                                                                                                                   
+count = 0
+xVals = []
+yVals = []
+
+#Constructing tk() object                                                                                                                                                                                         
+root = Tk()
+root.geometry('1920x1600')
+root.configure(background='pink')
+root.title("Python GUI")
+root.title("Python GUI")
+
+#Adding Top level menu options                                                                                                                                                                                    
+menu_bar = Menu(root)
+root.configure(menu=menu_bar)
+
+file_menu = Menu(menu_bar)
+file_menu.add_command(label = "Browse", command=browse_button)
+file_menu.add_separator()
+file_menu.add_command(label = "Crop", command=crop_button)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=exit)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
 root.mainloop()
-=======
-#File       
