9b326c00a35a2f03c855499992bd6203948e99f1
jclaun1 <jclaun1@umbc.edu>
2018-08-21 16:18:57 -0400

    Create ssh_OS.py

---
+++ PiComms/ssh_OS.py
@@ -0,0 +1,13 @@
+import os
+
+#assign Pi's IP address
+#zeroIP = x
+"""
+os.system("ssh pi@p1.local")
+os.system("%INSERT PI PASSWORD HERE%")
+"""
+
+
+"""
+os.system("ssh pi@p1.local\n %INSERT PI PASSWORD HERE%")
+"""
