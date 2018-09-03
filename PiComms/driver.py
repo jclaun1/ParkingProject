54863fd19e84c6d5efcbeae35bbb4f4fcc04667c
jclaun1 <jclaun1@umbc.edu>
2018-09-02 20:04:22 -0400

    Create driver.py

---
+++ PiComms/driver.py
@@ -0,0 +1,31 @@
+import sys
+import pexpect
+
+user = 'pi'
+password = 'clusterhat'
+host = 'p1.local'
+fout = open('mylog.txt', 'wb')
+
+def dossh(user, password, host):
+  try:
+    child = pexpect.spawn('ssh %s@%s' % (user,host))
+
+    child.logfile = sys.stdout
+    child.logfile_send = fout
+
+    child.expect(['pi@p1', pexpect.EOF])
+    child.sendline('python Capture.py')
+    
+    child.expect(['pi@p1', pexpect.EOF])
+    child.sendline('scp /home/pi/pixels.txt')
+    
+    child.close()
+    
+  except Exception as error:
+    print "Errors otw"
+    print error
+    sys.exit
+  
+
+
+dossh(user, password, host)
