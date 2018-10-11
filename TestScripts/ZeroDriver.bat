9b132ec16014e6d35e2d955480b1fe23b6212238
jclaun1 <jclaun1@umbc.edu>
2018-10-11 14:23:03 -0400

    Update ZeroDriver.bat

--- TestScripts/ZeroDriver.bat
+++ TestScripts/ZeroDriver.bat
@@ -3,7 +3,7 @@
 set MAX_NUMS = 500
 
 for /l %%locNum in (1, 1, %MAX_NUMS) do {
-  #set resultArray = system.callSystem("python Capture.py locNum")
-  python Capture.py %%locNum
-  ./Analyze
+  set resultArray = system.callSystem("python Capture.py %%locNum")
+  #python Capture.py %%locNum
+  ./Analyze %resultArray
 }
